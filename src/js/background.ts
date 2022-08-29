e8959f3c47859a19bacf13905728e2e167f6735e
yf.guojing <li.rongping@hellogroup.com>
2022-08-29 12:19:10 +0800

    gengixn

--- src/js/background.ts
+++ src/js/background.ts
@@ -1,9 +1,10 @@
 // chrome.contextMenus.create({title:"\u831c\u8363\u5b66\u4e60", onclick:function(info,tab){
 //     window.open('https://fanyi.mlife.top');
 // }});
-chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
-    if (message.type == "demo") {
-        sendResponse('pupopValue');
-    }
-    return true;
-});
+// chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+//     if (message.type == "demo") {
+//         sendResponse('pupopValue');
+//     }
+//     return true;
+// });
+export default {}
--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -65,7 +65,6 @@ module.exports = {
         use : [
           MiniCssExtractPlugin.loader,
           'css-loader',
-          'sass-loader'
         ]
       }
     ]
